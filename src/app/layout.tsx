1dad6ec8c4cb1a8de1103a5d2330d713cf40f38d
Gordon Julian Koehn <gordon@koehn.net>
2025-03-22 14:45:35 +0100

    title and profile picture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-mode="system" suppressHydrationWarning>
       <head>
         <link
           rel="apple-touch-icon"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
+import Image from 'next/image';
 
 export default function Index() {
   const allPosts = getAllPosts();
@@ -15,6 +16,25 @@ export default function Index() {
     <main>
       <Container>
         <Intro />
+        <div className="relative">
+          {/* Background cover image */}
+          <Image
+            src="/assets/blog/hello-world/cover.jpg"
+            alt="Cover"
+            fill
+            style={{ objectFit: 'cover', width: '100%' }}
+            className="object-cover"
+          />
+          {/* Profile picture overlay */}
+          <div className="absolute top-4 left-4 w-56 h-56 rounded-full overflow-hidden border-4 border-white">
+            <Image
+              src="/assets/profile/2.png"
+              alt="Profile Picture"
+              fill
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+        </div>
         <HeroPost
           title={heroPost.title}
           coverImage={heroPost.coverImage}
